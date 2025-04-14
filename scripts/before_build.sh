11f0207f3ef2df37c31908097baa99a6b222a4a1
Chris Cant <chris@phdcc.com>
2025-04-14 20:00:27 +0100

    Add scripts

---
+++ scripts/before_build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+echo "- COPYING USER LOGO from icons";
+
+mkdir www/images
+cp icons/modtools_logo.png www/images
+
+echo "- COPYING ICON FILES from icons/*/modtools.png";
+
+#mkdir platforms/android/res/drawable
+#cp icons/hdpi/modtools.png platforms/android/res/drawable-hdpi/icon.png
+cp icons/hdpi/modtools.png platforms/android/app/src/main/res/drawable-hdpi/icon.png
+#cp icons/mdpi/modtools.png platforms/android/res/drawable-mdpi/icon.png
+cp icons/mdpi/modtools.png platforms/android/app/src/main/res/drawable-mdpi/icon.png
+#cp icons/xhdpi/modtools.png platforms/android/res/drawable-xhdpi/icon.png
+cp icons/xhdpi/modtools.png platforms/android/app/src/main/res/drawable-xhdpi/icon.png
+#cp icons/xxhdpi/modtools.png platforms/android/res/drawable-xxhdpi/icon.png
+mkdir platforms/android/app/src/main/res/drawable-xxhdpi
+cp icons/xxhdpi/modtools.png platforms/android/app/src/main/res/drawable-xxhdpi/icon.png
+
+exit
